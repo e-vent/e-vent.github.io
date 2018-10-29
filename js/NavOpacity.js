9e1a6b42edd66422d8ec6505695149813415251e
Daniel Tang <danielzgtg.opensource@gmail.com>
2018-10-29 18:09:57 -0400

    Move to CSS classes in scripts

--- js/NavOpacity.js
+++ js/NavOpacity.js
@@ -1,29 +1,10 @@
 $(document).ready(function() {
+root=$("html");
 $(window).scroll(function() {
 if ($(this).scrollTop() > 190) {
-$('nav').css({'height':"41px",'background-color':"white",'color':"black","box-shadow":"-1px 3px 10px 1px rgba(143,141,143,0.6)"});
-$('#MainLogo').css({'opacity':1});
-$("nav span").css({"font-size":"17px","color":"black"})
-$("#logo").css({"opacity":"0"})
-$("#nav-j>span").css({"color":"white"})
-$("#nav-j").css({"background-color":"#A1A5FF"})
-//$("nav>li>a").hover(function(){
-  //  $(this).css("color", "#7C7FC1");
-    //}, function(){
-    //$(this).css("color", "black");
-//});
+root.addClass('nav-scrolled');
 } else {
-$('nav').css({'height':'56px','background-color':"transparent",'color':'white',"box-shadow":"none"});
-$('#MainLogo').css({'opacity':0});
-$("#logo").css({"opacity":"1"})
-$("nav span").css({"font-size":"19px","color":"white"})
-$("#nav-j>span").css({"color":"#A1A5FF","background-color":"transparent"})
-$("#nav-j").css({"background-color":"transparent"})
-//$("nav>li>a").hover(function(){
-  //  $(this).css("color", "#7C7FC1");
-    //}, function(){
-   // $(this).css("color", "white");
-//});
+root.removeClass('nav-scrolled');
 }
 });
 });
--- js/SizeScroll.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-    if ($(this).scrollTop() > 240) {
-    $('topnav').css('height', "56px");
-    } else {
-    $('topnav').css('height', "41px");
-    }
-    });
-    });
