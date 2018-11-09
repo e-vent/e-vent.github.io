888c55267fc2efd4c86310e8dea5245baa8ac5e4
Daniel Tang <danielzgtg.opensource@gmail.com>
2018-11-08 19:56:28 -0500

    Fix JS on Index

--- index/BgSwap.js
+++ index/BgSwap.js
@@ -1,10 +1,7 @@
-var i = 0;
+const Img = ["index/Bg1.jpg", "index/Bg1_2.jpg", "index/bg1_3.jpg"];
+const hero = document.getElementById("hero");
+let i = 0;
 setInterval(function() {
-    var hero = document.getElementById("hero");
-    var Img =["index/Bg1.jpg","index/bg1_2.jpg","index/bg1_3.jpg"];
-    hero.style.background = "url(" + Img[i] + ") no-repeat fixed center/cover";
-    i = i + 1;
-    if (i == Img.length) {
-    i =  0;
-      }
-}, 10000);
+hero.style.background = "url(" + Img[i] + ") no-repeat fixed center/cover";
+i = (i + 1) % Img.length;
+}, 2000);
--- index/Preload.js
+++ index/Preload.js
@@ -1,20 +1,18 @@
-
-var images = new Array()
+let images = [];
 function preload() {
-    for (i = 0; i < preload.arguments.length; i++) {
-        images[i] = new Image()
-        images[i].src = preload.arguments[i]
-    }
+for (let i = 0; i < preload.arguments.length; i++) {
+images[i] = new Image();
+images[i].src = preload.arguments[i];
+}
 }
 preload(
-    "index/bg2.jpg",
-    "index/bg3.jpg",
-    "index/bg1.jpg",
-    "index/bg1_2.jpg",
-    "index/bg1_3.jpg",
-    "logo/logovector1.svg",
-    "logo/logovector2.svg",
-    "logo/logovector3.svg",
-    "common/down-arrow-v2.png",
-)
-//--><!]]>
+"index/Bg2.jpg",
+"index/Bg3.jpg",
+"index/Bg1.jpg",
+"index/Bg1_2.jpg",
+"index/bg1_3.jpg",
+"logo/logovector1.svg",
+"logo/logovector2.svg",
+"logo/logovector3.svg",
+"common/down-arrow-v2.png",
+);
