6d107f40df72305ab3830f42eee00b9807bf563f
Daniel Tang <danielzgtg.opensource@gmail.com>
2018-11-09 16:46:59 -0500

    Fix navbar and body positioning

--- common/NavScroll.js
+++ common/NavScroll.js
@@ -1,12 +1,14 @@
+const root = $('html');
 function update() {
-var root=$('html');
 if (root.scrollTop() > 190) {
 root.addClass('nav2');
 } else {
 root.removeClass('nav2');
 }
 }
+const main = $('main');
 $(function() {
+main.addClass('nav3');
+update();
 $(window).scroll(update);
-update()
 });
