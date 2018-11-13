56d22795e9cc326144b4cdf6675dd502c3333363
Daniel Tang <danielzgtg.opensource@gmail.com>
2018-11-13 07:58:45 -0500

    Turn bgswap back on with 8s

--- index/BgSwap.js
+++ index/BgSwap.js
@@ -1,7 +1,7 @@
 const Img = ["index/Bg1.jpg", "index/Bg1_2.jpg", "index/bg1_3.jpg"];
-const hero = document.getElementById("hero");
+const hero = document.getElementsByTagName("main")[0];
 let i = 0;
 setInterval(function() {
 hero.style.background = "url(" + Img[i] + ") no-repeat fixed center/cover";
 i = (i + 1) % Img.length;
-}, 2000);
+}, 8000);
