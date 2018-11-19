e-vent/e-vent.github.io

commit 59ab80e9fbc0c33ee21e9c9d0e225d8c06737662

Get rid of JQuery

profile/page.js
@@ -1,3 +1,4 @@
+(function () {
 function listToRadioForm(l) {
 var text = "<form id='rf'>";
 var i;
@@ -44,3 +45,4 @@ document.getElementById('name').innerHTML='';
 document.location.href="index.html";
 });
 }
+})();

AboutUs.html
@@ -9,7 +9,6 @@
 <meta name="google-signin-scope" content="profile email">
 <meta name="google-signin-client_id" content="365679910149-388t7l2qc3kflnj52f01mk4bku5ffiis.apps.googleusercontent.com">
 <title>About US</title>
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 </head>
 <!--DO NOT EDIT ANYTHING BEFORE <main> (outside template.html) A COMPUTER WILL DELETE YOUR CHANGES!!!-->
 <body>
@@ -33,7 +32,7 @@
 </nav>
 </header>
 <main class="parallax"><!--buildscriptdivider$header-content-->
-<script src="common/NavScroll.js"></script>
+<script src="common/NavScroll.js" defer></script>
 <div style="text-align: center;"><h1 style="color:white;">Our misssion</h1></div>
 <div style="margin: 15px 325px 125px; text-align: center;"><p>E-vent's team is dedicated to delivering the best event planning experience you can imagine. Our programming skills ensure that E-vent is reliable and constantly updated and improved. Our designers work every day to make E-vent's user interface easier to use and have the best event scheduling experience in the market.</p></div><br>
 <div style="text-align:center;margin: 0 25px 4px;"><img src="https://scontent.fxds1-1.fna.fbcdn.net/v/t1.0-9/46040710_2213844811959881_3955827920869523456_o.jpg?_nc_cat=104&_nc_ht=scontent.fxds1-1.fna&oh=871ca7d6370a52bcea730fedfb79c5a1&oe=5C7395C4" width="750" usemap="#planetmap"></div>

ContactUs.html
@@ -9,7 +9,6 @@
 <meta name="google-signin-scope" content="profile email">
 <meta name="google-signin-client_id" content="365679910149-388t7l2qc3kflnj52f01mk4bku5ffiis.apps.googleusercontent.com">
 <title>Contact Us</title>
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 </head>
 <!--DO NOT EDIT ANYTHING BEFORE <main> (outside template.html) A COMPUTER WILL DELETE YOUR CHANGES!!!-->
 <body>
@@ -33,7 +32,7 @@
 </nav>
 </header>
 <main class="parallax"><!--buildscriptdivider$header-content-->
-<script src="common/NavScroll.js"></script>
+<script src="common/NavScroll.js" defer></script>
 <div class="h">
 <h1 class="t">CONNECT WITH US</h1>
 </div>

common/NavScroll.js
@@ -1,14 +1,14 @@
-const root = $('html');
+(function () {
+const root = document.documentElement;
 function update() {
-if (root.scrollTop() > 190) {
-root.addClass('nav2');
+if (root.scrollTop > 190) {
+root.classList.add('nav2');
 } else {
-root.removeClass('nav2');
+root.classList.remove('nav2');
 }
 }
-const main = $('main');
-$(function() {
-main.addClass('nav3');
+const main = document.getElementsByTagName('main')[0];
+main.classList.add('nav3');
 update();
-$(window).scroll(update);
-});
+window.addEventListener('scroll', update);
+})();

help.html
@@ -9,7 +9,6 @@
 <meta name="google-signin-scope" content="profile email">
 <meta name="google-signin-client_id" content="365679910149-388t7l2qc3kflnj52f01mk4bku5ffiis.apps.googleusercontent.com">
 <title>Help</title>
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 </head>
 <!--DO NOT EDIT ANYTHING BEFORE <main> (outside template.html) A COMPUTER WILL DELETE YOUR CHANGES!!!-->
 <body>

index.html
@@ -9,7 +9,6 @@
 <meta name="google-signin-scope" content="profile email">
 <meta name="google-signin-client_id" content="365679910149-388t7l2qc3kflnj52f01mk4bku5ffiis.apps.googleusercontent.com">
 <title>E-vent</title>
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 </head>
 <!--DO NOT EDIT ANYTHING BEFORE <main> (outside template.html) A COMPUTER WILL DELETE YOUR CHANGES!!!-->
 <body>
@@ -33,9 +32,9 @@
 </nav>
 </header>
 <main class="parallax"><!--buildscriptdivider$header-content-->
-<script src="index/Preload.js"></script>
-<script src="common/NavScroll.js"></script>
-<script src="index/BgSwap.js"></script>
+<script src="index/Preload.js" defer></script>
+<script src="common/NavScroll.js" defer></script>
+<script src="index/BgSwap.js" defer></script>
 <section id="s1">
 <div id="logo" class="logo"><a href="#"></a></div>
 <br/>
@@ -90,7 +89,7 @@ Description
 </div>
 </a>
 </template>
-<script src="index/Carousel.js"></script>
+<script src="index/Carousel.js" defer></script>
 </div>
 <div class="downbutton"><a href="#section4"></a></div>
 </section>

index/BgSwap.js
@@ -1,7 +1,9 @@
-const Img = ["index/Bg1.jpg", "index/Bg1_2.jpg", "index/bg1_3.jpg"];
-const hero = document.getElementsByTagName("main")[0];
+(function () {
+const Img = ['index/Bg1.jpg', 'index/Bg1_2.jpg', 'index/bg1_3.jpg'];
+const main = document.getElementsByTagName('main')[0];
 let i = 0;
 setInterval(function() {
-hero.style.background = "url(" + Img[i] + ") no-repeat fixed center/cover";
+main.style.background = 'url(' + Img[i] + ') no-repeat fixed center/cover';
 i = (i + 1) % Img.length;
 }, 8000);
+})();

index/Carousel.js
@@ -1,5 +1,6 @@
-const eventRoot = $('#newevents')[0];
-const template = $('#evt-template')[0].content.querySelector(".evt");
+(function () {
+const eventRoot = document.getElementById('newevents');
+const template = document.getElementById('evt-template').content.querySelector(".evt");
 const baseURL = 'https://danielzgtg.duckdns.org';
 function openXHR(endpoint, goodCb, badCb) {
 const xhr = new XMLHttpRequest();
@@ -9,6 +10,7 @@ xhr.onload = function() {
 goodCb(xhr.responseText)
 };
 if (badCb != null) {
+// noinspection UnnecessaryLocalVariableJS
 const f = function() {
 badCb()
 };
@@ -26,7 +28,7 @@ console.log('Count response ' + text);
 const count = parseInt(text);
 console.log('Have ' + count + ' events');
 if (count > nextEventID + 10) {
-console.log('Skipping old events to keep up with feed')
+console.log('Skipping old events to keep up with feed');
 nextEventID = count - 5
 }
 if (nextEventID < count) {
@@ -53,3 +55,4 @@ setTimeout(doupdate, 2000)
 })
 }
 setTimeout(doupdate, 1000);
+})();

commit 5f25c0a41e41a1bcb2c01903d66802c3b348f919

Fix profile page JS scope
A regression was introduced by 59ab80e which sandboxed all the
variables. The profile page needs to break the sandbox for the
callbacks.

profile/page.js
@@ -1,4 +1,3 @@
-(function () {
 function listToRadioForm(l) {
 var text = "<form id='rf'>";
 var i;
@@ -45,4 +44,3 @@ document.getElementById('name').innerHTML='';
 document.location.href="index.html";
 });
 }
-})();